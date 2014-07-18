d3f72325244188515f9c56395901cec07f2f87c8
timelyportfolio <kent.russell@timelyportfolio.com>
2014-07-18 14:39:14 -0500

    set up spin.js to return spinner inside anonymous function

--- inst/spin.js
+++ inst/spin.js
@@ -1,3 +1,1 @@
-var htmlWidgets = typeof(htmlWidgets) == "undefined" ? {} : htmlWidgets;
-
-htmlWidgets["payload-id"] = new Spinner(payload.config).spin(document.getElementById(payload.id));
+return new Spinner(payload.config).spin(document.getElementById(payload.id));
