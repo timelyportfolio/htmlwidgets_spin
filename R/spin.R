b39e5ec0cf7054ea7f310d13cf69b242d102f58c
timelyportfolio <kent.russell@timelyportfolio.com>
2014-07-18 14:04:44 -0500

    combine all attrs in structure to clarify code

--- R/spin.R
+++ R/spin.R
@@ -2,10 +2,13 @@
 spin <- function(...){
   params <-  list(...)
   params = Filter(Negate(is.null), params)
-  s <- structure(params, class = c('spin', 'htmlwidget'))
-  attr(s,"jsfile") <- "http://timelyportfolio.github.io/htmlwidgets_spin/inst/spin.js"
-  attr(s,"config") <- "http://timelyportfolio.github.io/htmlwidgets_spin/inst/spin.yaml"
-  attr(s,"package") <- NULL
+  s <- structure(
+    params
+    , class = c('spin', 'htmlwidget')
+    , jsfile = "http://timelyportfolio.github.io/htmlwidgets_spin/inst/spin.js"
+    , config = "http://timelyportfolio.github.io/htmlwidgets_spin/inst/spin.yaml"
+    , package <- NULL
+  )
   return(s)
 }
 
