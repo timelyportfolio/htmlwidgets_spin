e15f43d9cfbb5124ee19844757ffcc87eb167d39
timelyportfolio <kent.russell@timelyportfolio.com>
2014-07-31 23:14:30 -0500

    add an `return` to see if we can use `window.HTMLWidgets.widgets[i].returnValue` as our repository of returned objects

--- inst/spin.js
+++ inst/spin.js
@@ -2,6 +2,6 @@ HTMLWidgets.widget({
   name: "spin",
   type: "output",
   renderValue: function(el, data) {
-    new Spinner(data.config).spin(document.getElementById(data.id))
+    return new Spinner(data.config).spin(document.getElementById(data.id))
   }
 });
